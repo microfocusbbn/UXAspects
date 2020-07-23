820d99d5c3623ae759e4485bd66bfdf857e2cba9
Erik Littwin <erik.littwin@microfocus.com>
2020-07-23 10:25:33 +0200

    add indent to conditions and groups

--- src/components/logical-expression-builder/leb-condition/leb-condition.component.ts
+++ src/components/logical-expression-builder/leb-condition/leb-condition.component.ts
@@ -61,6 +61,8 @@ export class LebConditionComponent implements OnChanges, OnInit, OnDestroy {
 
     @Input() indent: number = 0;
 
+    @Input() indent: number = 0;
+
     public fields: FieldDefinition[];
     public operators: OperatorDefinition[];
 
